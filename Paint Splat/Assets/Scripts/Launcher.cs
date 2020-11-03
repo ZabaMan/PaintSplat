ed320c79e148af1fbce8db5c5f4ffcb815df056b
starrrrria <guok@tcd.ie>
2020-11-03 10:15:20 +0000

    step 4

--- Paint Splat/Assets/Scripts/Launcher.cs
+++ Paint Splat/Assets/Scripts/Launcher.cs
@@ -65,6 +65,12 @@ namespace Com.MyCompany.MyGame
 
         public override void OnJoinedRoom()
         {
+            Debug.Log("We load the 'GameRoom' ");
+
+
+            // #Critical
+            // Load the Room Level.
+            PhotonNetwork.LoadLevel("GameRoom");
             Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
         }
 
