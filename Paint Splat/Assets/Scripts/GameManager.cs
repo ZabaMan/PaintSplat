6baafcc2894035c2d5a956c35e31a3b8a2c22498
molonepa <molonepa@tcd.ie>
2020-11-09 17:12:07 +0000

    instantiate board scene object

--- Paint Splat/Assets/Scripts/GameManager.cs
+++ Paint Splat/Assets/Scripts/GameManager.cs
@@ -20,6 +20,9 @@ namespace Com.MyCompany.MyGame
         public Text Score;
         public Text userName;
 
+        [Tooltip("The prefab to use for representing the board")]
+        public GameObject boardPrefab;
+
         #region Photon Callbacks
 
         private void Start()
@@ -39,6 +42,18 @@ namespace Com.MyCompany.MyGame
                 else
                 {
                     Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+                }
+            }
+            if (boardPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> boardPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    Debug.LogFormat("We are Instantiating LocalBoard from {0}", SceneManagerHelper.ActiveSceneName);
+                    PhotonNetwork.InstantiateSceneObject("Board", new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
                 }
             }
         }
